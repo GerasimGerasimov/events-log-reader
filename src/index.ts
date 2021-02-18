75b444f1a80bc37c6be7672d58f4f6eaa29c362b
Gerasim Gerasimov <dialix@yandex.ru>
2021-02-18 18:29:52 +0700

    временно отключил "лишнее"

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 console.log('event-log-reader started');
 import fs = require('fs');
 
-import { getConfigFile } from './helpers/utils';
-import { HttpServer } from './server/httpserver';
+//import { getConfigFile } from './helpers/utils';
+//import { HttpServer } from './server/httpserver';
 import { TDBReader } from './db/dbreader';
 
-const settings = JSON.parse(fs.readFileSync(getConfigFile(), 'utf8'));
-const Server: HttpServer = new HttpServer(settings.HOST.port);
+//const settings = JSON.parse(fs.readFileSync(getConfigFile(), 'utf8'));
+//const Server: HttpServer = new HttpServer(settings.HOST.port);
 const DBReader = new TDBReader();
 
 (async () => {
