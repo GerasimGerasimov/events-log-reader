e7893fe851aa5d52e719c6ece4eb8f8c06d87e9f
Gerasim Gerasimov <dialix@yandex.ru>
2021-02-18 18:25:30 +0700

    готовлюсь читать макс и мин даты в таблице

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,18 @@
 console.log('event-log-reader started');
 import fs = require('fs');
 
-import TDAO  from './DB/DAO';
-import EventsRepositoty from './DB/EventsRepository';
-
-import { HttpServer } from './server/httpserver';
 import { getConfigFile } from './helpers/utils';
+import { HttpServer } from './server/httpserver';
+import { TDBReader } from './db/dbreader';
 
 const settings = JSON.parse(fs.readFileSync(getConfigFile(), 'utf8'));
 const Server: HttpServer = new HttpServer(settings.HOST.port);
+const DBReader = new TDBReader();
 
-
-/*
-async function main(){
-  const dao: TDAO = new TDAO('./db/database.sqlite3');
-  const eventsRepo = new EventsRepositoty(dao);
-}
-
-main()
-console.log('stop')
-*/
-
-/*
+(async () => {
   try {
-
-    console.log('start to create table')
-    await eventsRepo.createTable();
-
-    console.log('start row count:', await eventsRepo.getRowCount())
-    
-    async function* asyncGenerator(max: number) {
-      let i = 0;
-      while (i < max) {
-        yield i++;
-      }
-    }
-
-    console.log('start to create records')
-
-  
-    await dao.run('BEGIN TRANSACTION');
-    var index:number = 0;
-    for await (let i of asyncGenerator(2048)) {
-      const eventSource = createEvent(faker.random.number({
-        'min': 0,
-        'max': EventsMap.size - 1})
-        )
-        if (eventSource !== undefined) {
-          const {datetime, tag, details} = {... eventSource}
-          const { type, initialValue, comment, todo } = { ... details}
-          const event: IEvent = {
-            date: datetime,
-            type,
-            tag,
-            details : {
-              initialValue,
-              comment,
-              todo
-            }
-          }
-          await eventsRepo.create(event)
-          console.log(`create record: ${i} ${type} ${tag}`)
-          
-
-        } else {
-          console.log(`Error on record ${i}`)
-        }
-    }
-
-  } catch(e) {
-    console.log('Error :', e)
+    console.log('end row count:', await DBReader.getRowCount());
+  } catch (e) {
+    console.log(e)
   }
-    await dao.run('COMMIT TRANSACTION');
-    console.log('end row count:', await eventsRepo.getRowCount())
-    console.log('event-log-sqlite stoped');
-
-*/
+})()
